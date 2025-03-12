a125e7a1f4648a8e053edd93e12ec19109bed0aa
bben-ni <195986731+bben-ni@users.noreply.github.com>
2025-03-12 00:52:11 +0000

    Submit

--- if_conditional.py
+++ if_conditional.py
@@ -2,18 +2,18 @@
 birth_year = int(input("Please enter your birth year (e.g., 1990): "))
 
 # Step 2: Determine the generation based on the birth year
-If birth_year < 1946:
-generation = "Silent Generation"
+if birth_year < 1946:
+    generation = "Silent Generation"
 elif birth_year < 1965:
-generation = "Baby Boomer"
+    generation = "Baby Boomer"
 elif birth_year < 1981:
-generation = "Generation X"
+    generation = "Generation X"
 elif birth_year < 1997:
-generation = "Millennial"
+    generation = "Millennial"
 elif birth_year < 2013:
-generation = "Generation Z"
+    generation = "Generation Z"
 else :
-generation = "Generation Alpha"
+    generation = "Generation Alpha"
 
 # Step 3: Print the identified generation
 print(f"You belong to: {generation}")
